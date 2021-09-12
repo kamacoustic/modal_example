15a98786b2bae485ef97f7439fdd34b34eeaebba
kamacoustic <75150502+kamacoustic@users.noreply.github.com>
2021-09-12 14:48:06 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import Modal from './components/Modal';
 import './App.css';
 
 function App() {
+
+  const [openModal, setOpenModal ] = useState(false)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Click button to open Modal</h1>
+     <button 
+      className="openModalBtn"
+      onClick={() => {setOpenModal(true)}}>Open
+      </button>
+      {openModal && <Modal closeModal={setOpenModal} />}
+     
     </div>
   );
 }
