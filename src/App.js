84d52bc6a7375f6c3c197f06184e3b11c00b0488
kamacoustic <75150502+kamacoustic@users.noreply.github.com>
2021-09-13 00:29:49 -0400

    updated to easier integrate to portfolio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Modal from './components/Modal';
+import doompic from './components/images/doomzu.png'
 import './App.css';
 
 function App() {
@@ -9,12 +10,20 @@ function App() {
 
   return (
     <div className="App">
-     <h1>Click button to open Modal</h1>
-     <button 
-      className="openModalBtn"
-      onClick={() => {setOpenModal(true)}}>Open
-      </button>
-      {openModal && <Modal closeModal={setOpenModal} />}
+     <h1>Click image to open Modal</h1>
+     <div className="img_container">
+      <img
+        src={doompic}
+        alt="doomzu"
+        onClick={() => {setOpenModal(true)}}>
+        </img>
+      </div>
+      {openModal && <Modal
+         modal_link={"http://themojoworks.com/doomzu/index.html"}
+         closeModal={setOpenModal}
+         modal_title={"FYI!!!!"}
+         modal_p={"Page will open in new tab"}
+          />}
      
     </div>
   );
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { ExternalLink } from 'react-external-link'
 import './Modal.css'
 
-const Modal = ( { closeModal }) => {
+const Modal = ( { closeModal, modal_link, modal_title, modal_p }) => {
+
+
+
+    React.useEffect(() => {
+        window.addEventListener('click', (e) => {
+            const modalBackground = document.getElementById("modal")
+
+
+            if (e.target == modalBackground) {
+                closeModal(false)
+            }
+        })
+    })
 
     return(
-        <div className="modalBackground">
+        <div className="modalBackground" id="modal">
             <div className="modalContainer">
-            <div className="titleCloseBtn">
-                <button onClick={() => closeModal(false)}>X</button>
-            </div>
+            
                 <div className="title">
-                    <h1>Are you sure you want to continue?</h1>
+                    <h1>{modal_title}</h1>
                 </div>
                 <div className="body">
-                    <p>Page will open in new tab</p>
+                    <p>{modal_p}</p>
+                    <h3>{modal_link} </h3>
                 </div>
                 <div className="footer"> 
+                
                     <button onClick={() => closeModal(false)} id="cancelBtn">Cancel</button>
-                    <button>Continue</button>
+                   
+                    <ExternalLink href={modal_link}>
+                        <button>Continue</button>
+                    </ExternalLink>
                  </div>
                       
                     
